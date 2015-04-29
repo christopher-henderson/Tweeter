4bfa3d95809c1fc466394478a370c2f41cf90fd5
Christopher Henderson <chris.henderson@saifeinc.com>
2015-04-29 16:25:23 -0700

    Added series

--- src/database/Models.py
+++ src/database/Models.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////{HOME}/database/tweeter.db'.
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(254), unique=True)
@@ -16,9 +16,9 @@ class User(db.Model):
     def __init__(self, name, email):
         self.name = name
         self.email = email
-
-class Event(db.Model):
     
+class Event(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     recipient = db.Column(db.String(15))
     pub_date = db.Column(db.DateTime, nullable=False)
@@ -29,9 +29,11 @@ class Event(db.Model):
     tweet = db.relationship('Tweet', backref=db.backref('events', lazy='dynamic'))
     playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'))
     playlist = db.relationship('Playlist', backref=db.backref('events', lazy='dynamic'))
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
+    series = db.relationship('Playlist', backref=db.backref('events', lazy='dynamic'))
     shuffle = db.Column(db.Boolean)
 
-    def __init__(self, user, tweet, pub_date, recipient=None, consumed=False, playlist=None, shuffle=False):
+    def __init__(self, user, tweet, pub_date, recipient=None, consumed=False, playlist=None, shuffle=False, series=None):
         self.user = user
         self.tweet = tweet
         self.pub_date = pub_date
@@ -39,23 +41,24 @@ class Event(db.Model):
         self.consumed = consumed
         self.playlist = playlist
         self.shuffle = shuffle
+        self.series = series
 
 class Tweet(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(140), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('tweets', lazy='dynamic'))
     playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'))
     playlist = db.relationship('Playlist', backref=db.backref('tweets', lazy='dynamic'))
-    
+
     def __init__(self, content, user, playlist=None):
         self.content = content
         self.user = user
         self.playlist = None
 
 class Playlist(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -64,4 +67,12 @@ class Playlist(db.Model):
     def __init__(self, name, user):
         self.name = name
         self.user = user
-        
+
+class Series(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('series', lazy='dynamic'))
+    
+    def __init__(self, user):
+        self.user = user
